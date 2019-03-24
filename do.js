a4fc8e904fad8dc6624fceb712f78b6c8b5faeb9
Evan Genest <genest.1@osu.edu>
2019-03-24 06:10:44 -0500

    Add button & query by SELECTOR.

--- do.js
+++ do.js
@@ -1,5 +1,12 @@
-console.log("Script loaded.");
+console.log("Script loaded for bear.");
 
 const greet =()=>{
 	console.log("Ben!  Ben Greenberg!");
+}
+
+
+const bFr = document.getElementById('button-frame');
+var buffoon = document.querySelector('button');
+buffoon.onclick =()=> {
+	console.log('clicked ' + (new Date()).getSeconds());
 }
