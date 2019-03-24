2f4cb487318a74d23ab0f57ea7ecad07bd00e725
Evan Genest <genest.1@osu.edu>
2019-03-24 06:40:44 -0500

    Hook two buttons to js: one ID, one querySelector. Fix ONLOAD.

--- do.js
+++ do.js
@@ -1,12 +1,27 @@
+document.body.onload =()=>{   
+/***********************************************/
 console.log("Script loaded for bear.");
 
-const greet =()=>{
-	console.log("Ben!  Ben Greenberg!");
-}
+greet();
 
-
-const bFr = document.getElementById('button-frame');
+var bFr = document.getElementById('button-frame');
 var buffoon = document.querySelector('button');
 buffoon.onclick =()=> {
 	console.log('clicked ' + (new Date()).getSeconds());
 }
+
+bFr.setAttribute('background-color', 'red');
+
+/***********************************************/
+} 
+
+var greet =()=>{
+	console.log("Ben!  Ben Greenberg!");
+}
+
+/*
+To do:
+On Load is not being used right.  
+Review "Placement of the javascript" and also scope or hoisting, I think!
+The greeter() faild when inside the onload.
+*/
