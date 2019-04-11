7cb1861dbc5aab67a73a62100d31491a556579f9
Evan Genest <genest.1@osu.edu>
2019-04-11 07:46:16 -0500

    Write recursive for 3threes; return base case only.

---
+++ three3s.js
@@ -0,0 +1,15 @@
+
+let haveThree =(nums)=>{
+	return false;
+}
+
+
+
+
+haveThree([3, 1, 3, 1, 3]) // true
+haveThree([3, 1, 3, 3]) // false
+haveThree([3, 4, 3, 3, 4]) // false
+
+//Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
+// https://codingbat.com/prob/p109783
+// April 11, 2019
