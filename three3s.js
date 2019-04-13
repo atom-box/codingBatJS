e8853cb71dc71fbc315dfb0c65a365f8efeba8d9
Evan Genest <genest.1@osu.edu>
2019-04-13 06:25:47 -0500

    Fix the next loop: correctly holds up two adjacent array memebers."

--- three3s.js
+++ three3s.js
@@ -4,24 +4,28 @@ let haveThree =(nums)=>{
 	let touch = false; // adjacents?  todo!
 	let alot = false;  // three or more?
 	const WANTED = 3;
-	let next = null;
+	let next = 0;
 	// can refactor to map filter reduce TODO TODO TODO !
 	for (let n in nums){
 		if (nums[n] === WANTED){
 			++occurances;
 		}
-		next = n + 1;
+		next = next + 1;
+		console.log(`Next is set to [${next}]`);
+		console.log(`Now comparing [${nums[n]}] and [${nums[next]}]`);
+
+
 		if (n < nums.length - 1 ){
 			if (
 				nums[n] === WANTED &&
-				nums[n+1] === WANTED
+				nums[next] === WANTED
 			){
 				touch = true;
-				console.log(`TOUCHING, THESE TOUCH ${nums[n]} and ${nums}`);
+				// console.log(`TOUCHING, THESE TOUCH ${nums[n]} and ${nums}`);
 			} else if (
 				nums[n] !== WANTED || 
-				nums[n+1] !== WANTED
-			) { console.log(`No problem, these aren't touching ${WANTED} [${nums[n]}][${nums}]`)} else { console.log(`Never gets here I hope.`)}
+				nums[next] !== WANTED
+			) { console.log(`No problem, these aren't touching [${nums[n]}] and [${nums[next]}] [${nums[n]}][${nums}]`)} else { console.log(`Never gets here I hope.`)}
 		}
 	}
 
@@ -46,14 +50,18 @@ let haveThree =(nums)=>{
 
 
 console.log(haveThree([3, 1, 3, 993])) // false
-console.log(haveThree([55,  , 55, 5, 900, 3, 3, 43,  12, 166, 300])) // false
+// console.log(haveThree([55,  , 55, 5, 900, 3, 3, 43,  12, 166, 300])) // false
 console.log(haveThree([3, 4, 3, 3, 4])) // false
-console.log(haveThree([55, 72, 100, 3.0, 3, 3])) // false
-console.log(haveThree([3, 4, 5, 3, 6, 6, 6, 6, 6, 88, 78987, 1.3, 3])) // true
+// console.log(haveThree([55, 72, 100, 3.0, 3, 3])) // false
+// console.log(haveThree([3, 4, 5, 3, 6, 6, 6, 6, 6, 88, 78987, 1.3, 3])) // true
 console.log(haveThree([3, 1, 3, 1, 3])) // true
 console.log(haveThree([3, 4, 5, 3, 6, 6, 6, 6, 6, 88, 78987, 1.3, 3])) // true
-console.log(haveThree([3, 1, 3, 1, 3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,1000])) // true
+// console.log(haveThree([3, 1, 3, 1, 3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,1000])) // true
 
 //Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
 // https://codingbat.com/prob/p109783
 // April 11, 2019
+
+// not working, the referring to [next]
+// 1) try a for loop
+// 2) try printing multiple print statements
