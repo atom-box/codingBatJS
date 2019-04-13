53dd7d24b65b657deeb4a029ba5257917b84b870
Evan Genest <genest.1@osu.edu>
2019-04-13 08:15:04 -0500

    Remove debugger printstatements.

--- three3s.js
+++ three3s.js
@@ -7,7 +7,6 @@ let haveThree =(nums)=>{
 	let neighbor = null;
 	// can refactor to map filter reduce TODO TODO TODO !
 
-	console.log(`Array to compare ---> ${nums}`);
 	for (let n in nums){
 
 		// This tallyer must come before break statement
@@ -17,11 +16,9 @@ let haveThree =(nums)=>{
 
 		// don't eval final array spot; she has no neighbor!
 		if (n == nums.length - 1){
-			console.log('Declining eval of final position');
 			break;
 		}
 		neighbor = neighbor + 1;
-		console.log(`Now comparing [${nums[n]}] and [${nums[neighbor]}]`);
 
 
 		// if (n < nums.length - 1 ){  // todo todo change back to if (n < nums.length - 1 )
@@ -30,7 +27,6 @@ let haveThree =(nums)=>{
 				nums[neighbor] === WANTED
 			){
 				touch = true; // set flag
-				console.log(`O--u--c--h, THESE TOUCH ${nums[n]} and ${nums[neighbor]}`);
 			} else if (          // =========================== NOT
 				nums[n] !== WANTED || 
 				nums[neighbor] !== WANTED
@@ -44,7 +40,6 @@ let haveThree =(nums)=>{
 
 	// take tally and make a boolean
 	if (occurances >= 3){ alot = true}
-	console.log('Result:');
 	console.log(`Occurances ${occurances} Touch:${touch}  aLot${alot}   Array${nums}`);
 	if (alot && !touch){
 		return true; // todo always returning now
@@ -63,35 +58,33 @@ let haveThree =(nums)=>{
 let testArray = [3, 3, 993];
 let pre = `Overall return for [${testArray}] is: `;
 console.log(pre + haveThree(testArray));
-console.log(`\n\n\n`);
+console.log(`\n`);
 
 testArray = [3, 1, 3, 1,  3, 0, 3, 3];
 pre = `Overall return for [${testArray}] is: `;
 console.log(pre + haveThree(testArray));
-console.log(`\n\n\n`);
+console.log(`\n`);
 
 
 testArray = [3, 1, 3, 1, 3, 3, 0, ,];
 pre = `Overall return for [${testArray}] is: `;
 console.log(pre + haveThree(testArray));
-console.log(`\n\n\n`);
+console.log(`\n`);
 
 testArray = [3, 1, 3, 1, 3,6,3,6,3,6,3,6,3,6,3, 6,3,6,3,6,3,6,3,1000,3,3];
 pre = `Overall return for [${testArray}] is: `;
 console.log(pre + haveThree(testArray));
-console.log(`\n\n\n`);
+console.log(`\n`);
 
 testArray = [2, 2, 3, 55, 2 , 55, 5, 900, 3, 43,  12, 166, 300,3 ,2];
 pre = `Overall return for [${testArray}] is: `;
 console.log(pre + haveThree(testArray));
-console.log(`\n\n\n`);
+console.log(`\n`);
 
 testArray = [3, 4, 5, 3, 6, 6, 6, 6, 6, 88, 78987, 1.3, 3];
 pre = `Overall return for [${testArray}] is: `;
 console.log(pre + haveThree(testArray));
-console.log(`\n\n\n`);
-
-
+console.log(`\n`);
 
 /*
 Should return:
