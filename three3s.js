5743119ddf1f8c4d7c0118f73d6fce1440d84392
Evan Genest <genest.1@osu.edu>
2019-04-13 07:07:55 -0500

    Make loop BREAK instead of evaluating final array member.

--- three3s.js
+++ three3s.js
@@ -4,30 +4,38 @@ let haveThree =(nums)=>{
 	let touch = false; // adjacents?  todo!
 	let alot = false;  // three or more?
 	const WANTED = 3;
-	let next = 0;
+	let neighbor = null;
 	// can refactor to map filter reduce TODO TODO TODO !
+
+	console.log(`Array to compare ---> ${nums}`);
 	for (let n in nums){
+		// don't eval final array spot; she has no neighbor!
+		if (n == nums.length - 1){
+			console.log('Declining eval of final position');
+			break;
+		}
 		if (nums[n] === WANTED){
 			++occurances;
 		}
-		next = next + 1;
-		console.log(`Next is set to [${next}]`);
-		console.log(`Now comparing [${nums[n]}] and [${nums[next]}]`);
+		neighbor = neighbor + 1;
+		console.log(`Now comparing [${nums[n]}] and [${nums[neighbor]}]`);
 
 
-		if (n < nums.length - 1 ){
+		if (n < 3){  // todo todo change back to if (n < nums.length - 1 )
 			if (
 				nums[n] === WANTED &&
-				nums[next] === WANTED
+				nums[neighbor] === WANTED
 			){
 				touch = true;
 				// console.log(`TOUCHING, THESE TOUCH ${nums[n]} and ${nums}`);
 			} else if (
 				nums[n] !== WANTED || 
-				nums[next] !== WANTED
-			) { console.log(`No problem, these aren't touching [${nums[n]}] and [${nums[next]}] [${nums[n]}][${nums}]`)} else { console.log(`Never gets here I hope.`)}
+				nums[neighbor] !== WANTED
+			) { console.log(`No problem, these aren't touching [${nums[n]}] and [${nums[neighbor]}]`)
+			} else { 
+				console.log(`Never gets here I hope.`)}
+			}
 		}
-	}
 
 
 	// take tally and make a boolean
@@ -58,7 +66,7 @@ console.log(haveThree([3, 1, 3, 1, 3])) // true
 console.log(haveThree([3, 4, 5, 3, 6, 6, 6, 6, 6, 88, 78987, 1.3, 3])) // true
 // console.log(haveThree([3, 1, 3, 1, 3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,6,3,1000])) // true
 
-//Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
+//Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are neighbor to each other.
 // https://codingbat.com/prob/p109783
 // April 11, 2019
 
