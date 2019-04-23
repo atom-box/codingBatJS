e68d24be6cfbad41be35d351af53436f870fa708
Evan Genest <genest.1@osu.edu>
2019-04-23 07:56:07 -0500

    Rough in the TRY CATCH for errors.

--- maxMod5.js
+++ maxMod5.js
@@ -5,6 +5,26 @@ Given two int values, return whichever value is larger. However if the two value
 */
 // Evan Genest, 4/2019, twitter@mistergenest
 
+
+let maxMod5 = (a, b)=>{
+	try {
+		if (a === b){
+			return 0;
+		} else if (){
+
+		} else if (){
+
+		} else {
+			throw new Error("Should never see this.");
+		}
+	catch(e){
+		console.log(e);
+	}
+}
+
+
+
+
 console.log(maxMod5(2, 3)); // 3
 console.log(maxMod5(6, 2)); // 6
 console.log(maxMod5(3, 2)); // 3
