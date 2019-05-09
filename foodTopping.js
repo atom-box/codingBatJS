41fd63c1359cd2efdb03e35e52069de7d62ecaab
Evan Genest <genest.1@osu.edu>
2019-05-09 08:12:02 -0500

    Make lone function loop through the passed-in object.

--- foodTopping.js
+++ foodTopping.js
@@ -4,7 +4,13 @@ Given a map of food keys and topping values, modify and return the map as follow
 All meals must have "bread" with "butter".
 
 */
-
+let topping1 =(o)=>{
+	console.log(`----`);
+	for (let i in o){
+		console.log(`${i}: ${o[i]}`);
+	}
+	return o["ice cream"] + ` is the topping.`;
+}
 
 
 console.log(topping1({"ice cream": "peanuts"})); // {"bread": "butter", "ice cream": "cherry"}
