976d3552c5e87366480d6623738425c5c58e7f72
Evan Genest <genest.1@osu.edu>
2019-05-09 19:40:42 -0500

    Function returns 'function' rather than the integer.

--- thisSummer.js
+++ thisSummer.js
@@ -1,14 +1,26 @@
 // just writing a getter-setter
 
-function sum(nums) {
+function sum() {
 	let keepGoing = nums.length,
 	tally = 0;
 	while (keepGoing--){
-		console.log('Index is ' + keepGoing + 'Number to add is ' + nums[keepGoing]);
-		tally += nums[keepGoing];
+		console.log('Index is ' + keepGoing + 'Number to add is ' + this.nums[keepGoing]);
+		tally += this.nums[keepGoing];
 	}
+	return tally;
 }
 
-console.log(sum([3, 10])); // 13
-console.log(sum([44, 3, 3])); // 50
-console.log(sum([1, 0, 1])); 
+shape1 ={nums: [3, 10], dummy: null, showTotal: sum}; // 13
+shape2 ={nums: [44, 3, 3], dummy: null, showTotal: sum}; // 50
+shape3 ={nums: [1, 0, 1], dummy: null, showTotal: sum}; 
+
+console.log(shape1.showTotal);
+console.log(shape2.showTotal);
+console.log(shape3.showTotal);
+
+
+
+
+
+// Refactor the sum to be a method passed in to the object and then called as an object method 
+
