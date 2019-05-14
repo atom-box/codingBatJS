55f608987cc89056d1524bd34adb8d24896b3760
Evan Genest <genest.1@osu.edu>
2019-05-14 13:15:34 -0500

    BreadAdder can unshift a static object into the array.

--- foodTopping.js
+++ foodTopping.js
@@ -20,17 +20,22 @@ let fixIceCream =(o)=>{
 	}
 };
 
-// Used inside of SOME()
-// This conditional returns boolean T/F 
-// if objectBREAD exists, and has butter
-/////////return HASBREADKEY  && BREADKEYPROPERTYISBUTTER
-/////////return 
 // Pass it an array-of-objects
 // It returns either nothing or single object: {bread: butter}
 // depeninding on whether the latter is absent
-// TODO right now it always always adds bread object
 function breadAdder(arr){
-	return {"pork-roll-egg": "cheese"}
+	// if (arr.some(obj => obj.bread === 'butter')){
+	// todo just trying to print for now
+	console.log(`Here are some things AA${arr}AA BB${arr[0]}BB CC${arr[0]['popcorn']}CC`);
+	if (true){
+		console.log('Now adding BREAD.')
+		return {'bread': 'butter'}
+	} else if (arr.length) {
+		console.log('Found nought in the bread adder func.');
+		return {'kidney': 'pie'};
+	} else {
+		return {"something went": "wrong"}
+	}
 } 
 
 
@@ -39,7 +44,8 @@ function breadAdder(arr){
 // Prints them 1 at a time.
 function showMenu(arrayOfObjects){
 	console.log(`----`);
-	for (let i in arrayOfObjects){
+	var i = arrayOfObjects.length;
+	while (i--){
 		for (let k in arrayOfObjects[i]){
 			console.log(`${k} with ${arrayOfObjects[i][k]}`);
 		}
@@ -52,20 +58,20 @@ function vetTheToppings(menu1) {
 	let menu2 = [], menu3 = [];
 	menu2 = menu1.map(fixIceCream);
 	// menu3 = menu2.unshift(breadAdder(menu2));
-	menu2.unshift(breadAdder(menu2));
+	menu2.unshift({'stretch pants': 'legs'});
 	return menu2;
 }
 
 
-let monday = [{'ice cream': 'peanuts'}, {'popcorn': 'yeast' }, {'ice cream': 'butter'}, {'bread': 'cheese'}];
-let tuesday = [{'apple': 'butter'}, {'ice cream': 'chives'}, {'pancake': 'syrup'}, {'bloody mary': 'slim jim'}];
+let monday = [{'popcorn': 'yeast' }, {'ice cream': 'butter'}, {'ice cream': 'peanuts'}, {'bread': 'cheese'}];
+let tuesday = [{'popcorn': 'yeast' },{'apple': 'butter'}, {'ice cream': 'chives'}, {'pancake': 'syrup'}, {'bloody mary': 'slim jim'}];
 let wednesday = [{'potato': 'salt'}];
 let thursday = [{'coffee': 'oat milk'}, {'asparagus': 'soy sauce'}];
-let fixedMenu = [];
+let fixedMenu = [{a: 'b'}];
 
 showMenu(monday); //
 fixedMenu = vetTheToppings(monday); 
 showMenu(fixedMenu);
 showMenu(tuesday); // 
-fixedMenu = vetTheToppings(tuesday); // 
+fixedMenu = vetTheToppings(tuesday) // 
 showMenu(fixedMenu);
