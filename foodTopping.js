e64e9b82464fcb6c579cc6456ec71c3138f2364f
Evan Genest <genest.1@osu.edu>
2019-05-14 08:00:49 -0500

    Reconfigure flow as follows
    1) Write TOPPING1 so it calls MAP with FIXICECREAM
    2) (coming soon, similar TOPPING1 call of SOME on CHECK FOR BREAD
    3) Write show menu function, use wherever print statment needed
    4) Expand test arrays to have more test items

--- foodTopping.js
+++ foodTopping.js
@@ -1,37 +1,54 @@
 /*
-Given a map of food keys and topping values, modify and return the map as follows: 
+Given an array of objects where each object is made of food keys and topping values, modify and return the array of objects as follows: 
 "ice cream" can only have "cherry". 
 All meals must have "bread" with "butter".
 TODO: refactor to use the map function
 */
-let fixIceCream =()=>{
-	// this will be a mapping of the object
-	this["ice cream"] = "onions";
-}
 
-let ensureBreadPresence =()=>{
-	// todo write a conditional onto this
-	this.taco = "Tabasco";
-}
-
-
-
-
-
-let topping1 =(menu)=>{
-	console.log(`----`);
-	// menu = ensureBreadPresence(menu);
-	// menu = fixIceCream(menu);
-	for (let i in menu){
-		// if (menu.hasOwnProperty(i)){
-			console.log(i + `: ` + menu[i] + ` in topping1`);
-		// }
+// Used inside of MAP
+// Takes one object
+// If IC present, sets to Cherry, w/o checking
+let fixIceCream =(o)=>{
+	if (o.hasOwnProperty('ice cream')){
+			o['ice cream'] = 'onions';
+			console.log('Found onions!');
+			return o;
+	} else if (o) {
+		return o;
+	} else {
+		return null;
 	}
-	return `Stars thar: ${menu}`;
+};
+
+// Used inside of SOME()
+// This conditional returns boolean T/F 
+// if objectBREAD exists, and has butter
+
+
+//  Used whenever you need to show the array of objects
+//  Takes an A of O
+// Prints them 1 at a time.
+function showMenu(arrayOfObjects){
+	for (let i in arrayOfObjects){
+		console.log(Object.keys(arrayOfObjects[i]));
+	}
 }
 
 
-console.log(topping1({"ice cream": "peanuts"})); // {"bread": "butter", "ice cream": "cherry"}
-console.log(topping1({})); // {"bread": "butter"}
-console.log(topping1({"pancake": "syrup"})); // {"bread": "butter", "pancake": "syrup"}
+// Menu: Array of objects.
+function topping1(menu) {
+	console.log(`----`);
+	showMenu(menu);
+}
+
+
+let monday = [{'ice cream': 'peanuts'}, {'popcorn': 'yeast' }, {'ice cream': 'butter'}, {'bread': 'cheese'}];
+let tuesday = [{'apple': 'butter'}, {'ice cream': 'chives'}, {'pancake': 'syrup'}, {'bloody mary': 'slim jim'}];
+let wednesday = [{'potato': 'salt'}];
+let thursday = [{'coffee': 'oat milk'}, {'asparagus': 'soy sauce'}];
+
+showMenu(monday); //
+topping1(monday); 
+showMenu(tuesday); // 
+topping1(tuesday); // 
 
