8c10a00c89d0016ce5113a8e49e569b74d2f260b
Evan Genest <genest.1@osu.edu>
2019-06-15 06:33:25 -0500

    Subfunction can triple string. Main function returns dummy array.

---
+++ copies3.js
@@ -0,0 +1,34 @@
+/* copies3.js
+ Challenge: Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together. 
+ Evan Genest
+ June 15, 2019 
+ */
+"use strict";
+
+let multify = (sum, item) =>{
+// Accept a string.
+// Return a string that is 3x the first)
+	const FACTOR = 3;
+	for (let i = FACTOR; i > 0; i--){
+		sum += item;
+	};
+	return sum;
+}
+
+
+function copies3 (items){
+	// Accept an array.
+	// Return an array where each item was tripled.
+	return ['a', 'b', 'c'];
+}
+
+console.log(multify('', 'carter'));
+console.log(multify('', 'z'));
+console.log(multify('', 'leek'));
+
+
+console.log(copies3(["a", "bb", "ccc"])); // ["aaa", "bbbbbb", "ccccccccc"]
+console.log(copies3(["24", "a", ""])); // ["242424", "aaa", ""]
+console.log(copies3(["hello", "there"])); // ["hellohellohello", "theretherethere"]
+
+
