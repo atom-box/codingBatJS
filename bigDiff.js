cfad6c2aad499ee0f08611fd0826d9594dc9a127
Evan Genest <genest.1@osu.edu>
2019-06-16 11:22:21 -0500

    Works for actual numbers.  Edge cases get a bit weird.

--- bigDiff.js
+++ bigDiff.js
@@ -9,19 +9,17 @@ function findSmallest(nums){
 	return nums.reduce((a, b)=>{return (a<b)? a:b}  );
 }
 
+function bigDiff(nums){
+	return findBiggest(nums) - findSmallest(nums);
+}
 
-
-console.log(findBiggest([10, 3, 5, 6])); // 10
-console.log(findBiggest([7, 2, 11, 9])); // 11
-console.log(findBiggest([12, 10, 7, 2])); // 12
-console.log(findSmallest([3, 10, 4, 5, 6])); // 3
-console.log(findSmallest([7, 2, 11, 9])); // 2
-console.log(findSmallest([12, 10, 7, 2])); // 2
-
-
-// bigDiff([10, 3, 5, 6]) // 7
-// bigDiff([7, 2, 10, 9]) // 8
-// bigDiff([2, 10, 7, 2]) // 8
+console.log(bigDiff([10, 3, 5, 6])); // 7
+console.log(bigDiff([7, 2, 10, 9])); // 8
+console.log(bigDiff([0, -3, 0, 2])); // 5
+console.log(bigDiff([7, 2, "froggiest", 9])); // expected: 5 actual: 0
+console.log(bigDiff([2, NaN, 7, 2])); // 5
+console.log(bigDiff([2, 10, null, 2])); // wanted: 8 actual ten
+console.log(bigDiff([1, undefined, null, -1])); // expected: 2, actual 1
 
 /*
 Do this with reduce two times.  Max, min.  
