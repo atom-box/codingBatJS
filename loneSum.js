ab0004f291f9fe27c707aff59f2058d3e631884f
atom-box <genest.1@osu.edu>
2019-06-17 17:06:03 -0500

    Convert any number of args to an Array object in main funciton.

--- loneSum.js
+++ loneSum.js
@@ -1,6 +1,7 @@
 // Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum.
 
 function isCopycat(num, nums){
+	// Returns true if num is found in nums.
 	// Returns: boolean.
 	// Args: Int, Array of Ints
 	return nums.some(
@@ -8,14 +9,22 @@ function isCopycat(num, nums){
 		);
 }
 
+function loneSum(){
+	let nums = Array.from(arguments);
+	return(`U passed in ${nums.length} numbers.`)
+}  
+
 let temperatures = [ 56,47,38,57,46,36,66,44,88,77,99,44];
 console.log(isCopycat( 44, temperatures)); // true
 console.log(isCopycat(222, temperatures)); //false
 console.log(isCopycat(56, temperatures)); //true
 
-// loneSum(1, 2, 3) // 6
-// loneSum(3, 2, 3) // 2
-// loneSum(3, 3, 3) // 0
+
+
+
+console.log(loneSum(1, 2, 3, 44, 44)); // 6
+console.log(loneSum(3, 2, 3)); // 2
+console.log(loneSum(3, 3, 3)); // 0
 
 
 
