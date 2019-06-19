5c4edd65d7dd77200afd80af973716d92202af2d
Evan Genest <genest.1@osu.edu>
2019-06-19 07:49:39 -0500

    Haspeer() is working. When integrated with loneSum, too many numbers are allowed into the sum.

--- loneSum.js
+++ loneSum.js
@@ -11,8 +11,8 @@ function hasPeer(index, arr){
 	// console.log(`Length of array after splice: ${arr.length}`);
 	// console.log(`Value of cipher is ${cipher}`);
 	return arr.some((element)=>{
-		console.log(`Checking ${element} vs ${cipher} whose types are ${typeof element} ${typeof cipher}`);
-		console.log(element == cipher);
+		// console.log(`Checking ${element} vs ${cipher} whose types are ${typeof element} ${typeof cipher}`);
+		// console.log(element == cipher);
 		return (element == cipher);
 	});
 }
@@ -21,30 +21,20 @@ function loneSum(){
 	let nums = Array.from(arguments);
 	// return(`U passed in ${nums.length} numbers.`)
 	let result = nums.reduce((acc, item, index, arr)=>{
-		console.log(`Comparing ${item}.  IsCopycat? ${isCopycat(item, arr)}!`);
-		return acc += ((isCopycat(item, nums))? item : 0) ;  
-	}, 555);
+		console.log(`Comparing ${item}.  Has peer? ${hasPeer(index, arr)}!`);
+		return acc += ((hasPeer(index, arr))? 0 : item) ;  
+	}, 0);
 	return result;
 }  
 
-// let temperatures = [ 56,47,38,57,46,36,66,44,88,77,99,44];
-// console.log(isCopycat( 44, temperatures)); // true
-// console.log(isCopycat(222, temperatures)); //false
-// console.log(isCopycat(56, temperatures)); //true
 
 console.log(hasPeer(1 , [33, 9, 44, 9, 55])); // 2
 console.log(hasPeer(2 , [33, 9, 44, 9, 55])); // 2
 console.log(hasPeer(3 , [33, 9, 44, 9, 55])); // 2
+console.log('======================');
 
-// console.log(hasPeer(0 , [1, 2, 3, 44, 44])); // 2
-// console.log(hasPeer(1 , [1, 2, 3, 44, 44])); // 2
-// console.log(hasPeer(2 , [1, 2, 3, 44, 44])); // 2
-// console.log(hasPeer(3 , [1, 2, 3, 44, 44])); // 2
-// console.log(hasPeer(4, [1, 2, 3, 44, 44])); // 2
-
-
-// console.log(loneSum(1, 2, 3, 44, 44)); // 6
-// console.log(loneSum(3, 2, 3)); // 2
-// console.log(loneSum(3, 3, 3)); // 0
+console.log(loneSum(1, 2, 3, 44, 44)); // 6
+console.log(loneSum(3, 2, 3)); // 2
+console.log(loneSum(3, 3, 3)); // 0
 
 
