fc972f8fe33dfb5e7472f0d8c18a1525f71225f3
Evan Genest <genest.1@osu.edu>
2019-07-11 06:38:01 -0500

    Works.

--- recur-count9s.js
+++ recur-count9s.js
@@ -3,8 +3,7 @@
 
 function arrayCount9(nums){
 	var sum = 0,
-	piece = null,
-	isNine = false;
+	piece = null;
 
 	// checks digit for nine-ness
 	function isNine(n) {
@@ -17,7 +16,16 @@ function arrayCount9(nums){
 		return 0;
 	} else if (nums.length > 0){
 		piece = nums.shift();
-		console.log(piece);
+		if (isNine(piece)){
+			return 1 + arrayCount9(nums);
+		} else if (!isNine(piece)) {
+			return 0 + arrayCount9(nums);
+			/*
+				TODO REFACTOR TO NOT HAPPEN TWICE; MAKE A SUBFUNC.
+			*/
+		} else {
+			return NaN;
+		}
 		return 1 + arrayCount9(nums);
 	} else {
 		return -1;
@@ -26,7 +34,17 @@ function arrayCount9(nums){
 
 
 console.log(arrayCount9([1, 2, 9])) // 1
+console.log(`----`);
 console.log(arrayCount9([9, 1, null, 9, 0])) // 2
-console.log(arrayCount9([9, 1, 9, 3, 9])) // 3
+console.log(`----`);
+
+console.log(arrayCount9([9, 1, 0, 99, 9, 9, 9])) // 4
 
 // Solved as recursive function
+/* To solve things as recursive functions:
+
+1) Your base case probably returns NADA.
+2) Your other case may return RETURN 1 + recurs(nums)
+3) You may pop off a piece, store it in a var, use it
+4) If you went that route a CHECK FOR RAN OUT OF ROAD may be neccessary.
+*/
