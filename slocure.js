3e081db11b35a91e23e5ea06fbab22e1929c2e79
Evan Genest <genest.1@osu.edu>
2019-07-24 09:59:14 -0500

    Show that CONSTANT can be shadowed.

--- slocure.js
+++ slocure.js
@@ -1,12 +1,25 @@
 /*Write a function THAT RETURNS A FUNCTION. Notice the scope  */
+/*   See "Closures" page 125, Javascript Pocket Reference */
 
-let s = "global scope"
+// CRAZY:  USING CONSTANT HERE DOESNT AFFECT LATER SUBSEQUENT ew-SCOPE
+const s = "global scope"
 
 function check1() {
-	let s = "local scope";
+	let s = "local scope here";
 	function sayScope() {return s;}
 	// DON'T FORGET THE FOLLOWING!
 	return sayScope;
 }
 
+function check2(){
+	let s = "local scope also";
+	function sayScope(){
+		return s;
+	}
+	return sayScope();
+}
+
+
+console.log(s);
 console.log(check1()());
+console.log(check2());
