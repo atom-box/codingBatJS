a0a80cbdcf6729135c6b8c9d0dfaf646a9a1632a
Evan Genest <genest.1@osu.edu>
2019-08-06 11:22:05 -0500

    Made more tests. Passed.

--- sameEnd.js
+++ sameEnd.js
@@ -1,6 +1,9 @@
 /* Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string. */
 
+// https://codingbat.com/prob/p126880
+
 function aInB(a, b){
+	// console.log(`received ${a} <-and-> ${b}.`);
 	let aL = a.length,
 	bL = b.length;
 	if (aL > bL){
@@ -23,3 +26,7 @@ function endOther (s1, s2){
 console.log(endOther("Hiabc", "abc")); // true
 console.log(endOther("AbC", "HiaBc")); // true
 console.log(endOther("abc", "abXabc")); // true
+console.log(endOther("x", "X")); // true
+console.log(endOther("abc", "abXabc ")); // false
+console.log(endOther("AbC.", "HiaBc!")); // false
+console.log(endOther("abc", "abXab c")); // false
