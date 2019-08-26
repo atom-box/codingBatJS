c5a4fc71ca37179d95a5b887b1aeb21eaf102313
Evan Genest <genest.1@osu.edu>
2019-08-26 09:45:11 -0500

    Add a map function.

--- rAddStar.js
+++ rAddStar.js
@@ -1,5 +1,16 @@
 // Given a list of strings, return a list where each string has "*" added at its end.
 
+
+function addStar(cintas) {
+	var pprint = (s)=>{
+		console.log(s);
+	}
+	cintas.map(pprint);
+}
+
+
+
 addStar(["a", "bb", "ccc"]) // ["a*", "bb*", "ccc*"]
 addStar(["hello", "there"]) // ["hello*", "there*"]
 addStar(["*"]) // ["**"]
+
