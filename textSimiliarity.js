eedd30dd1a76c7458ac482eae5595dde92e84f04
Evan Genest <genest.1@osu.edu>
2019-10-12 16:03:07 -0500

    Improve the comments.

--- textSimiliarity.js
+++ textSimiliarity.js
@@ -29,135 +29,191 @@ What's going on here:
 // lint it
 // remove comments or make AirBnB style
 
-// Given two arrays of words, return total an object that has the number of orphans from each side.
-
-// (string s) -> {string with no non-alpha chars}
-function alphaSanitize(s) {
-	let dirtyChars = s.split('');
-	const reggie = /[A-z]/
-	let onlyAlphas = dirtyChars.filter( c => reggie.test(c) );
-	return onlyAlphas.join('');
-}
 
 
 
-// (string s)->{ several arrays of words, returns a final clean array of words }
-function toWords(s) {
-	const words = {};
+/*
+** A looper to show the 
+** array members during de-bugging
+*/
 
-	// 1a. split the string to words on ' ' as a delimiter
-	words.dirty = s.split(' '); // returns array of raw words
-
-	// 1b. returns array with spaces removed from word boundaries
-  words.trimmed = words.dirty.map((w)=>{return w.trim()} ); 
-  // console.log('Trimmed array is here: ' + words.trimmed);
-
-	// 2. create words.collapsed, an array that lacks any UNDEFINED array memebers
-  words.collapsed = words.trimmed.reduce( 
-    (x,y)=> {
-      if (y.length > 0){
-        x.push(y); 
-      }
-      return x;
+// () -> {}
+function thingsShow(arr) {
+    let many = arr.length;
+    console.log(`Array has the following ${many} things:`);
+    while (many > 0) {
+        many -= 1;
+        console.log(`!${arr[many]}!`);
     }
-    , []);
-
-	// 3. everyone lowercased
-  words.lowered = words.collapsed.map( (x)=>{return x.toLowerCase()} )  ;
-  // console.log('collapsed array is here: ' + words.collapsed);
-	// 4. strip beginning and endings that are non-alpha but ignore interior no-alpha.
-	words.justAlpha = words.lowered.map( item => alphaSanitize(item)  );
-  // TODO  TODO  TODO  TODO   toWords the ends
-  return words.justAlpha;
 }
 
-function scoreOneWord(s){
-	if (s.length < 3){
-		return 1;
-	} else if (s.length < 5) {
-		return 1; 
-	} else {
-		return 1;
-	}
+
+
+/*
+**
+**	Remove chars that aren't letters.
+** (string s) -> {string with no non-alpha chars} 
+*/
+//
+function alphaSanitize(s) {
+    let dirtyChars = s.split('');
+    const reggie = /[A-z]/
+    let onlyAlphas = dirtyChars.filter(c => reggie.test(c));
+    return onlyAlphas.join('');
 }
 
-// Accepts an array
-// Returns a copied-by-value array
+
+/*
+** Make word arrays from a text sample.
+** (string s)->{ several arrays of words, returns a final clean array of words }
+*/
+
+// 
+function toWords(s) {
+    const words = {};
+
+    // 1a. split the string to words on ' ' as a delimiter
+    words.dirty = s.split(' '); // returns array of raw words
+
+    // 1b. returns array with spaces removed from word boundaries
+    words.trimmed = words.dirty.map((w) => { return w.trim() });
+    // console.log('Trimmed array is here: ' + words.trimmed);
+
+    // 2. create words.collapsed, an array that lacks any UNDEFINED array memebers
+    words.collapsed = words.trimmed.reduce(
+        (x, y) => {
+            if (y.length > 0) {
+                x.push(y);
+            }
+            return x;
+        }, []);
+
+    // 3. everyone lowercased
+    words.lowered = words.collapsed.map((x) => { return x.toLowerCase() });
+    // console.log('collapsed array is here: ' + words.collapsed);
+    // 4. strip beginning and endings that are non-alpha but ignore interior no-alpha.
+    words.justAlpha = words.lowered.map(item => alphaSanitize(item));
+    // TODO  TODO  TODO  TODO   toWords the ends
+
+    return words.justAlpha;
+}
+
+
+
+
+
+/*
+** Give a more subtle score for orphans than '1'
+** Devalue the weight of small words
+**
+*/
+
+function scoreOneWord(s) {
+    if (s.length < 3) {
+        return 1;
+    } else if (s.length < 5) {
+        return 1;
+    } else {
+        return 1;
+    }
+}
+
+
+/*
+** To avoid mutatating the data
+** create a copy of the array
+** Accepts an array
+** Returns a copied-by-value array
+*/
+
 function makeLocal(a) {
-	let newArray = [];
-	const stop = a.length;
-	for (let i = 0; i < stop; i++) {
-		newArray.push(a[i]);
-	} 
-	return newArray;
+    let newArray = [];
+    const stop = a.length;
+    for (let i = 0; i < stop; i++) {
+        newArray.push(a[i]);
+    }
+    return newArray;
 }
+
+
+
+/*
+ ** Histogram bar draw.
+ **	Accepts an integer and a char
+ **  Returns nothing.
+ **  draws a bar in the console
+ ** TODO: return a string and let the main logic draw the bar wherever it wants
+ */
 
 function skidMark(n, c) {
-	let skid = '';
-	for(let i = 0; i < n; i++){
-		skid += c;
-	}
-	console.log(skid);
+    let skid = '';
+    for (let i = 0; i < n; i++) {
+        skid += c;
+    }
+    console.log(skid);
 }
 
-// Accepts: string of many words
-// Returns: array of only orphansl
-// Main logic.
-function oneWayScore(list1, list2){
-	// local copy
-	const oneWay = {};
 
-	oneWay.wordsI = makeLocal(list1);
-	oneWay.wordsJ = makeLocal(list2);
+/*
+ ** Args: two strings of words + spaces
+ ** Return: an integer score 
+ ** TODO: refactor so it returns an object that is array of orphans, array of found words, and integer number of original words
+ */
 
-
-
-	oneWay.orphans = [];
-	oneWay.founds = [];
-	console.log(`There are [${oneWay.orphans.length} orphans] [${oneWay.wordsI.length} list1] [${oneWay.wordsJ.length} list2]`);
-
-	for(let i = 0; i < oneWay.wordsI.length; i++){
-		for(let j=0; j < oneWay.wordsJ.length; j++){
-		skidMark(oneWay.wordsI.length, '|'); // debugger
-		skidMark(oneWay.wordsJ.length, '_'); // debugger
-			if(oneWay.wordsI[i] === oneWay.wordsJ[j]){
-				oneWay.founds.push( oneWay.wordsJ.splice(j,1) );
-				if(j > 1){
-					oneWay.orphans.push( oneWay.wordsJ.splice(0, j-1));
-				}
-				break;
-			}
-		}
-	}
-		console.log(`Orphans: ${oneWay.orphans}`);
-		console.log(`Founds: ${oneWay.founds}`);
-
-	// console.log(`There are [${oneWay.orphans.length} orphans] [${oneWay.wordsI.length} list1] [${oneWay.wordsJ.length} list2]`);
+function oneWayScore(list1, list2) {
+    // local copy
+    const oneWay = {};
+    oneWay.wordsI = makeLocal(list1);
+    oneWay.wordsJ = makeLocal(list2);
+    oneWay.orphans = [];
+    oneWay.founds = [];
+    console.log('Here are list 1: ');
+    thingsShow(oneWay.wordsI);
+    console.log('Here are list 2: ');
+    thingsShow(oneWay.wordsJ);
+    for (let i = 0; i < oneWay.wordsI.length; i++) {
+        for (let j = 0; j < oneWay.wordsJ.length; j++) {
+            skidMark(oneWay.wordsI.length, '|'); // debugger
+            skidMark(oneWay.wordsJ.length, '_'); // debugger
+            if (oneWay.wordsI[i] === oneWay.wordsJ[j]) {
+                oneWay.founds.push(oneWay.wordsJ.splice(j, 1));
+                if (j > 1) {
+                    oneWay.orphans.concat(oneWay.wordsJ.splice(0, j - 1));
+                }
+                break;
+            }
+        }
+    }
+    console.log(`Here are the founds: `);
+    thingsShow(oneWay.founds);
+    console.log(`Here are the orphans: `);
+    thingsShow(oneWay.orphans);
+    console.log(`math: ${oneWay.orphans.length} / (${oneWay.orphans.length} + ${oneWay.founds.length})`);
+    const localScore = oneWay.orphans.length / (oneWay.orphans.length + oneWay.founds.length);
+    console.log(`About to return ${localScore} as local score.`);
+    return localScore;
 }
 
-let words1 = toWords("a b c d e f");
-let words2 = toWords("x x x x x x x x x x a b c d x x x e x x x f x x x");
-console.log('1 words I downstairs: ' + words1);
-console.log('1 words J downstairs: ' + words2);
 
 
-oneWayScore(words1, words2);
-console.log('oooooooooooooOOooOoOOOoOoOooooooooooooo');
-console.log('2 words I downstairs: ' + words1);
-console.log('2 words J downstairs: ' + words2);
-
-oneWayScore(words2, words1);
-console.log('3 words I downstairs: ' + words1);
-console.log('3 words J downstairs: ' + words2);
-
-
-
-// Meaningless timestamp.
-console.log((new Date()).toLocaleTimeString());
+/*-----------M-A-I-N---------------------*/
 
 // remember: unlikeness is 1 for worst similarity, 0 for best similarity
-let unlikeness1 = 0, unlikeness2 =0;
-let wordsA =  toWords( "Have a 2good day today.");
+let unlikeness1 = 0,
+    unlikeness2 = 0,
+    totalUnlikeness = 0;
+let words1 = toWords("a b c d e f");
+let words2 = toWords("x x x x x x x x x x a b c d x x x e x x x f x x x");
+unlikeness1 = oneWayScore(words1, words2);
+console.log('\noooooooooooooOOooOoOOOoOoOooooooooooooo\n');
+console.log('oooooooooooooOOooOoOOOoOoOooooooooooooo\n');
+unlikeness2 = oneWayScore(words2, words1);
+totalUnlikeness = (unlikeness1 + unlikeness2) / 2;
+// Meaningless timestamp.
+console.log(`On a scale of 0 to 1, the badness of these two texts is ${totalUnlikeness}`)
+console.log((new Date()).toLocaleTimeString());
+
+
 
 
 /*   REAL DATA
@@ -202,6 +258,3 @@ Bonus Points:
 Package this application as a web service that performs the comparison in response to a POST request containing the two texts in the body of the payload.  You may use external libraries (i.e., flask).
 Let’s take it a step further and package the web service in a Docker container that can be built and run locally or pulled down and run via Docker Hub.
 */
-
-
-
