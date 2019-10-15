929ed33da48a9a84c32e3677db2d6d5a683817cf
Evan Genest <genest.1@osu.edu>
2019-10-15 10:34:25 -0500

    Make elaborate test data generator.

--- selectionSort.js
+++ selectionSort.js
@@ -17,14 +17,26 @@ for j starting with j = i + 1
 
 */
 
+
+
+/* This program:
+** (1) Generates a random array of ints
+** (2) Sorts that array with Selection Sort
+**
+**
+*/
 function makeArray(reps) {
-	let stringNum = "";
+	let stringNum = "", newNums = [], allNums = [];
 	while(reps--){
 		stringNum += (Math.random() * 100000000000000000).toString();
 	}
-	return stringNum;
-	// todo: convert to int
+	newNums = stringNum.split('');
+	allNums = newNums.map( val =>  Number(val) )
+	return allNums;
 }
 
+
+
+/*----M-A-I-N------------*/
 console.log( makeArray(22) );
 
