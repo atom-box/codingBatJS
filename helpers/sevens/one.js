908de6ea01758b137939653b12b5e5c8870d8402
Evan Genest <genest.1@osu.edu>
2019-10-18 11:48:36 -0500

    Realized the actual issue is 5 doesn't map 1:1 to 7. Remove 7 output for now.

--- helpers/sevens/one.js
+++ helpers/sevens/one.js
@@ -1,22 +1,23 @@
 let dice = {};
 
 dice.roll5 = function() {
-	return 5 * Math.random();
+	return Math.floor(5 * Math.random()) + 1;
 }
 
 // Assignment: implement this as a modification of output from dice.roll5 = function
 dice.roll7 = function() {
-	return 7 * dice.roll5() / 7;
+	return Math.round(7 * dice.roll5() / 7);
 }
 
 dice.show5 = function() {
 	// dice.el = document.getElementById('diceout');
-	console.log(dice.roll5());
+	dice.el.innerHTML = dice.roll5();
+	dice.el.style = `color: blue;`
 }
 
 dice.show7 = function() {
-	// dice.el = document.getElementById('diceout');
-	console.log(dice.roll7());
+	dice.el.innerHTML = `not straightfoward; still working on this`;
+	dice.el.style = `color: red;`
 }
 
 let sol1 = document.getElementById('solution1');
@@ -24,14 +25,14 @@ sol1.innerHTML = `
 <h3>Roll a dice with sides of</h3>
 <div class="btn-group">
     <button type="button" class="btn btn-primary btn-sm" onclick="dice.show5()" > five</button>
-    <button type="button" class="btn btn-primary btn-sm" onclick="dice.show7()" > seven</button>
+    <button type="button" class="btn btn-danger btn-sm" onclick="dice.show7()" > seven</button>
 </div>
-<p id="diceout"></p>
+<h3 id="diceout" style="color:transparent">null 99</h3>
 `;
 
 // take up some space
 dice.el = document.getElementById('diceout');
-dice.el.innerHTML = `<p style="color:transparent"> nada surf</p>`
+dice.el.innerHTML = `<h3 style="color:transparent; font-family: serif;"> nada surf</h3>`
 
 
 
