2dd4dee9743a9267c8391d2ec844508a4dc342e8
Evan Genest <genest.1@osu.edu>
2019-10-25 07:48:35 -0500

    Add CLI features 1) Allow command line word entry.  Running at the command line with NODE PROGRAMNAME.JS FOO BAR BAZ will analyze foo bar and baz 2) Running with a 't' flag will turn on effusive print statements for checking the logic 3) Running with no args will analyze three built in example words

--- helpers/sevens/streakyWord.js
+++ helpers/sevens/streakyWord.js
@@ -3,6 +3,27 @@ Find the longest stretch of chars without a repeat.
 First seen at National Decision Support on 10-24-2019.
 */
 
+	const app = {};
+	app.args = process.argv;
+	// To run in testing mode, invoke as node appyWord.js t
+	app.testing = false; // toggle print statements while running
+	// just runs three words at bottom of code
+	app.useBuiltIns = app.args.length < 4 ? true : false;
+	// if useBuiltIns is false, allows one or more words entered at the command line
+
+if (app.args[2] === 't') {
+	app.testing = true;
+	console.log('Running in TEST mode.')
+} else if (app.args.length < 3) {
+	console.log('Running in SILENT mode.')
+} else if (app.args.length > 3) {
+
+} else {
+	console.log('Extra arguments ignored.')
+}
+
+
+
 // Function to find first result where string of non repeaters is longest.
 // Accepts a string
 // Returns longest span as a string (first  only)
@@ -18,15 +39,23 @@ function uniq(s){
 	streak.last = 0; // last index of char that got repeated
 	streak.finalJ = 0;
 	streak.wordy = '';
+	let i, 
+	stop;
 
-	for (let i = 0, stop = streak.bigString.length; i < stop; i++){
+
+
+	for (i = 0, stop = streak.bigString.length; i < stop; i++){
 			for (let j = i; j < stop; j++ ){
-				console.log(`Now: here[${streak.bigString[j]}]  vs  starting[${streak.bigString[i]}]`);
+				if (app.testing) { // test mode true
+					console.log(`Now: here[${streak.bigString[j]}]  vs  starting[${streak.bigString[i]}]`);
+				}
 				// DOES FOUNDS-ARRAY CONTAIN THE CURRENT CHAR?
 				if (streak.founds.some((x)=> x === streak.bigString[j]   ) ) {
-					// TODO pseudo alert
-					console.log(`Say HEY!  ${streak.bigString[j]} is  ${streak.bigString[j]}  `);
-					break;
+						// TODO pseudo alert, in test mode only:
+					if (app.testing) { // test mode true
+						console.log(`Say HEY!  ${streak.bigString[j]} is  ${streak.bigString[j]}  `);
+					}
+						break;
 				}
 				// Put the current char into the FOUNDS array
 				streak.founds.push(streak.bigString[j]);
@@ -41,16 +70,29 @@ function uniq(s){
 			streak.smallString = streak.bigString.slice(streak.first, streak.last + 1);
 		}
 		streak.founds = [];
-	console.log(`Current longest ${streak.spannity}!`);
-	console.log(`${streak.bigString.slice(streak.first, streak.last + 1)}?`);
-	console.log(`${streak.spannity}!`);
+		if (app.testing) { // test mode true
+				console.log(`Current longest ${streak.spannity}!`);
+				console.log(`${streak.bigString.slice(streak.first, streak.last + 1)}?`);
+				console.log(`${streak.spannity}!`);
+		}
 	} // end I
 	return streak.smallString;
 }
 
-console.log(`=====We like ${uniq('baseball')}?================================`);
-console.log(`=====We like ${uniq('hootybobby')}?================================`);
-console.log(`=====We like ${uniq('hoorrahrrr')}?================================`);
+if (app.useBuiltIns) {
+	console.log(`=====We like ${uniq('baseball')}?================================`);
+	console.log(`=====We like ${uniq('ootybobbyy')}?================================`);
+	console.log(`=====We like ${uniq('hoorrahrrr')}?================================`);
+} else if (!app.useBuiltIns){
+	console.log(`app.args length is ${app.args.length}`)
+	while (app.args.length > 3) {
+		let w = app.args.pop();
+		console.log( `[Word] ${w} [Streak] ${uniq(w)}`);
+	}
+} else {
+	console.log(`Should never see this.`);
+
+}
 
 
 
