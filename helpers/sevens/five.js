46bc920023f148044bee2a348f1c999c79a74f25
Evan Genest <genest.1@osu.edu>
2019-11-13 14:23:03 -0600

    Line muncher seems to work.

--- helpers/sevens/five.js
+++ helpers/sevens/five.js
@@ -3,6 +3,30 @@ import ike from './eisenhower.js';
 // gotta run as node -r esm five.js to get import to work
 const longString = ike;
 
+ //   __                              
+ //  / _|                             
+ // | |_   _   _   _ __     ___   ___ 
+ // |  _| | | | | | '_ \   / __| / __|
+ // | |   | |_| | | | | | | (__  \__ \
+ // |_|    \__,_| |_| |_|  \___| |___/
+                                   
+function muncher (k, s){
+  let i = k,  // an index pointing to the raw string
+   recentCut = 0, // span from this to i defines substring
+   lines = [];
+  while (i < s.length){
+    while (s[i] !== ' ' && i > 0){
+      i--  
+    }
+    let nChars = i - recentCut;
+    lines.push(s.slice(recentCut, nChars)  );
+    recentCut = i;
+    i = i + k;
+  } 
+  return lines;
+}
+
+
 
  //                      _         
  //                     (_)        
@@ -17,6 +41,12 @@ if (process.argv[2] !== undefined) {
     switch (process.argv[2]) {
       case 'length': 
         console.log ( longString.length);
+        break;
+      case 'stringtoarray': 
+        const main = {};
+        main.lines = muncher(80, ike);
+        console.log(`First line is ${main.lines[0]}`);
+        console.log(`In all, made ${main.lines.length} lines`);
         break;
        default:
         console.log('no optional args recognized');     
