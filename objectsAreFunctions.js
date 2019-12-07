3661fcb950cfad104e31d5df31f044f9dae22c69
Evan Genest <genest.1@osu.edu>
2019-12-07 15:18:27 -0600

    Show enumerable functions of three variable types..

--- objectsAreFunctions.js
+++ objectsAreFunctions.js
@@ -23,15 +23,52 @@ function drawl(s){
 	}
 }
 
-console.log(drawl(process.argv[0]));
-console.log(drawl(process.argv[1]));
-console.log(drawl('foxy'));
-console.log(drawl(33));
-console.log(drawl(""));
-console.log(drawl(null));
+let now = new Date();
 
-
-
-for (p in drawl) {
-	console.log("P is " + p + " and is " + drawl[p]);
+console.log("Length of now.constructor " + now.constructor.length);
+for (p in now.constructor) {
+	console.log("P is " + p + " and is " + now.constructor[p]);
 }
+
+console.log("Length of drawl.constructor " + drawl.constructor.length);
+for (p in drawl.constructor) {
+	console.log("P is " + p + " and is " + drawl.constructor[p]);
+}
+
+
+// Just show OWN PROPERTIES of the funciton
+console.log(drawl(process.argv[0]));
+drawl.foo = "guitar";
+drawl.bar = [4, 5, 77, 345, 63, 4, 55]
+const myKeys = Object.keys(drawl);
+console.log("Length of object keys in myKeys " + myKeys.length);
+myKeys.forEach( (item) => {console.log(item)})
+
+// Show all properties, including inherited from prototype
+for (p in drawl){
+	console.log("Here:" + p + " is " + drawl[p]);
+}
+
+var myArray = [];
+var myMystery;
+
+console.log("myArray==========");
+for (property in myArray) {
+	console.log("Name: " + property + " Value: " + myArray[property]);
+}
+
+// console.log("mystery==============");
+// for (property in mystery) {
+// 	console.log("Name: " + property + " Value: " + myArray[property]);
+// }
+// ERROR!
+
+console.log("drawl================");
+for (property in drawl) {
+	console.log("Name: " + property + " Value: " + drawl[property]);
+}
+
+
+// (myKeys === undefined)? console.log("yuck") : myKeys.forEach(console.log(myKeys));
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function
