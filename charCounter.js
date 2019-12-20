bef7a3dfa39e09acfdd370f0aa38ee849af5ee22
Evan Genest <genest.1@osu.edu>
2019-12-19 22:57:24 -0500

    Add first two RegEx matches. Print results to console.

--- charCounter.js
+++ charCounter.js
@@ -4,20 +4,20 @@
  *  USEAGE, AT THE COMMMAND LINE TYPE node charCounter myfile.txt WHERE MYFILE.TXT IS ANY VALID PATH
  */
 
-var fs = require('fs');
-var contents = fs.readFileSync('./data/abc.txt', 'utf8');
+const fs = require('fs');
+const contents = fs.readFileSync('./data/abc.txt', 'utf8');
 console.log('modernity and');
 console.log('its discontents');
 
 // initialize an object to hold results
 const result = {
   rawText: '',
-  length: 0,
-  words: 0,
+  lines: 0,
+  allWords: 0,
   chars: {
-    all: 0, // but not spaces
+    all: 0, // including spaces
     spaces: 0,
-    alpha: 0,
+    alphas: 0,
     digits: 0,
     symbols: 0
   },
@@ -51,14 +51,23 @@ const find = {
 }
 
 // call the methods
-
+result.rawText = contents;
+result.chars.all = contents.length;
+result.chars.alphas = contents.match(/[A-z]/g).length;
+result.chars.spaces = contents.match(/\ /g).length;
+result.chars.digits = contents.match(/[0-9]/g).length;
 
 
 // print the results
-console.log(result.chars.all);
+console.log(`Number of lines: ${result.lines}`);
+console.log(`Number of characters (total): ${result.chars.all + result.chars.spaces}`);
+console.log(`Number of letters: ${result.chars.alphas}`);
+console.log(`Number of figures: ${result.chars.digits}`);
+console.log(`Number of other characters: ${result.chars.symbols}`);
+console.log(`Number of words: ${result.allWords}`);
 
 
 
 console.log(`Note: words are assumed to be any combination of chars bounded by a space, terminus, or the following symbols [.,?!"].  This definition of "word" will affect both the defition of word length as well as total number of words.`);
 
-// Keep track of coding time: 5pm to 630
+// Keep track of coding time: 5pm to 630, 1015 to 1056
