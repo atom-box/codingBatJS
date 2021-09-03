6288d7550c6f22fa1c9013bacf23300ea88d04cf
atom-box <atom_box@hotmail.com>
2021-09-03 15:31:34 -0500

    Add 'plow' footer. Add first challenge in JSON.

--- data/quiltData.mjs
+++ data/quiltData.mjs
@@ -1,11 +1,31 @@
 console.log('start quiltdata.');
 
-let articles = {
-    color: '#493',
+let articles = [
+{
+    backgroundColor: '#493',
+    textColor: '55b',
     question: 'How do you grab a div?',
     strategy: 'GetElementById()!',
     form: '<form><h1>Placeholder</h1><button>click</button></form>',
     linkToCodeRepo: 'https://github.com/atom-box/codingBatJS/blob/master/textSimilarity.html',
-    testElement: '<p class="blueBackground">dogfish - dogfish - dogfish - dogfish </p>'
-}
+    testElement: '<p class="blueBackground">dogfish - dogfish - dogfish - dogfish </p>',
+    inputs: [],
+    predictedOutputs: [],
+},
+{
+    backgroundColor: '#256',
+    textColor: '5b5',
+    question: 'Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.',
+    strategy: 'GetElementById()!',
+    form: '<form><h1>Placeholder</h1><button>click</button></form>',
+    linkToCodeRepo: 'https://github.com/atom-box/codingBatJS/blob/master/textSimilarity.html',
+    testElement: '<p class="blueBackground">dogfish - dogfish - dogfish - dogfish </p>',
+    inputs: [[1, 2, 3], [11, 5, 9], [2, 11, 3]],
+    predictedOutputs: [[3, 3, 3],  [11, 11, 11], [3, 3, 3]],
+    challengeSource: 'https://codingbat.com/prob/p146256',
+
+},
+
+]
+
 
