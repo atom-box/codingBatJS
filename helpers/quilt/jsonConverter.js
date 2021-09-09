1be9dec9e939bc42897774a2b1d2261f0985a61d
atom-box <atom_box@hotmail.com>
2021-09-09 07:11:17 -0500

    First commit.

---
+++ helpers/quilt/jsonConverter.js
@@ -0,0 +1,22 @@
+/*
+The best project is a tool that you actually need to use yourself.
+uThe White Sox stats here only export as a CSV
+https://www.baseball-reference.com/teams/CHW/2021.shtml
+I want them as a Json. Hence the need for the following converter.
+Evan Genest https://drupal.farm/
+*/
+
+// the JSON to build
+let outfile = {};
+// source of the CSV
+let inpath = "";
+
+const csvtojson = () => {
+    return "good news: this works";
+}
+
+// pseudocode
+/*
+Pass in a path as arg, open the path and store as a variable.
+A miracle happens.
+*/
