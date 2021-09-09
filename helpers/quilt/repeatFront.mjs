d4e29ce60baa240f166bcbb7939eaeb883cc1239
atom-box <atom_box@hotmail.com>
2021-09-09 13:31:59 -0500

    Works: run a recursive loop to repeat ever smaller chunks of front.

--- helpers/quilt/repeatFront.mjs
+++ helpers/quilt/repeatFront.mjs
@@ -1,16 +1,24 @@
-const repeatFront = (a, b) => {
-    return a + b;
+let repeatFront = (s, i) => {
+    if (typeof s !== 'string'){
+        return 'Try again: please enter a word';
+    }
+    if (s.length < 7){
+        return `${s.length} is too short`;
+    }
+
+    // take a chunk of the front
+    let firstBite = s.slice(0, i);
+
+    // recursive
+    let layBricks = (fragment) => {
+        if (fragment.length <= 1) {
+            return fragment;
+        }
+        was = fragment;
+        fragment = fragment.slice(0, fragment.length - 1);
+        console.log('was length ' + was.length + 'frag length: ' + fragment.length);
+        return was + layBricks(fragment);
+    }
+
+    return layBricks(firstBite);
 }
-
-
-
-
-// repeatFront.js
-// const price = document.querySelector('#price');
-// const output = document.querySelector('.price-output');
-
-// output.textContent = price.value;
-
-// price.addEventListener('input', function() {
-//   output.textContent = price.value;
-// });
