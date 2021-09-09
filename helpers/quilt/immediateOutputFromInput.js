cb29c2704ce73a269c2cee64dfd7f4a12eb9c047
atom-box <atom_box@hotmail.com>
2021-09-08 20:07:32 -0500

    Remove unused helper: immediateOutputFromInput.

--- helpers/quilt/immediateOutputFromInput.js
+++
@@ -1,8 +0,0 @@
-// DEPRECATED  todo
-
-let grub = (s)=>{
-    // let now = new Date();
-    // now = now.toLocaleString();
-    // console.log(`At {now} here is: {str1}`);
-    // return s + "-" + s;
-  }
